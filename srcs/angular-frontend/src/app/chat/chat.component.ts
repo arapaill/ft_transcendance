6fce4efbbfb3e36b2fa6203ea0bae2c034d1b748
CG-Walker <cogo97@hotmail.com>
2022-08-23 01:28:55 +0200

    Chat désormais responsive et placeholder de pong prends 100% de la taille possible

--- srcs/angular-frontend/src/app/chat/chat.component.ts
+++ srcs/angular-frontend/src/app/chat/chat.component.ts
@@ -21,7 +21,13 @@ export class ChatComponent implements OnInit {
       {
         userPseudo: "cgoncalv",
         userAvatar: "assets/avatar-placeholder-1.png",
-        text: "This is a test",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere volutpat metus, non convallis felis bibendum ut. Sed aliquet, neque eget pretium pretium, lorem mauris condimentum mi, auctor placerat eros justo sed ante. Ut sed mi est. Cras dictum ornare augue a cursus. Vivamus pulvinar, ex sit amet feugiat vehicula, tellus lorem gravida urna, at blandit ex mi rhoncus tellus. Aenean nec pharetra ante. Phasellus lobortis mi at ipsum varius interdum. Maecenas aliquet lacus sagittis enim commodo maximus. Nulla tristique lacinia nisi eu condimentum. Ut eu turpis eleifend, porta risus eu, venenatis erat. Ut eu purus ac eros pretium placerat et sed dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. ",
+        date: new Date()
+      },
+      {
+        userPseudo: "arapaill",
+        userAvatar: "assets/avatar-placeholder-2.png",
+        text: "This is another test",
         date: new Date()
       },
       {
