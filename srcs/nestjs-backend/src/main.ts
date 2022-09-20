cf644aef4c3730a0366c90debb4d54a073a62bf4
Julien André <ajuln@hotmail.fr>
2022-09-20 15:57:59 +0200

    ajout gitignore, changement de port du backend vers 3000

--- srcs/nestjs-backend/src/main.ts
+++ srcs/nestjs-backend/src/main.ts
@@ -5,7 +5,6 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'static'));
-  await app.listen(9876);
+  await app.listen(3000);
 }
 bootstrap();
