d68869392c6d9103a1f54b857ab3b628d0cdb88d
arapaill <arapaill@student.s19.be>
2022-09-30 10:03:25 +0200

    image uploader

--- srcs/angular-frontend/src/app/popup-modifier-profil/popup-modifier-profil.component.ts
+++ srcs/angular-frontend/src/app/popup-modifier-profil/popup-modifier-profil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-popup-modifier-profil',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopupModifierProfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  selectedFile!: File;
 
   ngOnInit(): void {
+  }
+  processFile(event : any){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.selectedFile = <File>event.target.files[0];
+  }
+  onUpload(){
+
   }
 
 }
