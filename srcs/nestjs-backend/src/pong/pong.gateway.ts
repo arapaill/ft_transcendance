95e3853502467e73aa10a196beeb3619e9e5d8ca
Julien André <ajuln@hotmail.fr>
2022-10-04 18:35:59 +0200

    multi up, reste spectateur

--- srcs/nestjs-backend/src/pong/pong.gateway.ts
+++ srcs/nestjs-backend/src/pong/pong.gateway.ts
@@ -1,5 +1,5 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { Game, GameState } from './pong.entity';
+import { Game, GameState, LeftOrRight } from './pong.entity';
 
 @WebSocketGateway({
   cors: {
@@ -26,27 +26,40 @@ export class PongGateway {
       }
       else if (this.games.get(socket_id).gameState == GameState.SEARCHING) {
         for (let [key, value] of this.games) {
-          if (key != socket_id && (value.gameState == GameState.WAITING || value.gameState == GameState.SEARCHING)) {
-            this.games.get(socket_id).addPlayer(key);                                               //Ce que je veux faire c'est chercher dans toutes les keys
-            this.games.get(socket_id).addPlayer(socket_id);                                         //Si jamais on peut trouver une key qui est en train de wait,
-            value = this.games.get(socket_id);                                                      //Si c pas le cas ben tu passe de SEARCHING a WAITING et t en liste d'attente
-            this.games.get(socket_id).gameState = GameState.MULTI;                                  //sinon ben lets go j'essaie de mettre le même game avec deux keys differentes dans la map
-          }                                                                                         //C geré de l'autre côte dans entity qui aura quel joueur avec la fonction addplayer
-          if (this.games.get(socket_id).gameState == GameState.SEARCHING)                           //Mais ca a pas l'air de fonctionner donc jsp si vous allez voir mais on sait jamais.
+          if (key != socket_id && value.gameState == GameState.WAITING) {
+            this.games.get(key).addPlayer(socket_id, LeftOrRight.RIGHT);
+            this.games.set(socket_id, this.games.get(key));
+            this.games.get(key).gameState = GameState.MULTI;
+          }
+          if (this.games.get(socket_id).gameState == GameState.SEARCHING) {
             this.games.get(socket_id).gameState = GameState.WAITING;
+            this.games.get(socket_id).addPlayer(socket_id, LeftOrRight.LEFT);
+          }
         }
-        this.games.get(socket_id).update(payload[0]); // PB se trouve ici malheureusement ;....:///
+        this.games.get(socket_id).update(payload[0]);
         client.emit('update', this.games.get(socket_id).returnData());
       }
       else if (this.games.get(socket_id).gameState == GameState.SPECTATING) { }
+      else if (this.games.get(socket_id).gameState == GameState.OVER) {
+        if (payload[0].ACTION == "QUIT") {
+          if (this.games.has(socket_id))
+            this.games.delete(socket_id);
+          this.games.set(socket_id, new Game(socket_id));
+          this.games.get(socket_id).gameState = GameState.MENU;
+        }
+        this.games.get(socket_id).update(payload[0]);
+        client.emit('update', this.games.get(socket_id).returnData());
+      }
       else {
+        if (this.games.get(socket_id).playerTwo != undefined) {
+          console.log(this.games.get(socket_id).playerTwo.socket);
+        }
         this.games.get(socket_id).update(payload[0]);
         client.emit('update', this.games.get(socket_id).returnData());
       }
     }
     else {
       this.games.set(socket_id, new Game(socket_id));
-      console.log(this.games.get(socket_id).gameState);
     }
   }
 }
