0cdfe7fa4c09cdd099c51846b21f7dbb631b672e
Julien André <ajuln@hotmail.fr>
2022-10-07 01:35:14 +0200

    pour le pong

--- srcs/nestjs-backend/src/pong/pong.gateway.ts
+++ srcs/nestjs-backend/src/pong/pong.gateway.ts
@@ -1,7 +1,6 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Game, GameState, LeftOrRight } from './pong.entity';
-import { GameHistoryService } from 'src/gameHistory/gameHistory.service';
-import { OngoingGameService } from 'src/ongoingGames/ongoingGames.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @WebSocketGateway({
   cors: {
@@ -14,9 +13,62 @@ export class PongGateway {
 
   private games: any;
 
-  constructor() {
+  constructor(private prisma: PrismaService) {
     this.games = new Map();
+
   }
+
+  @SubscribeMessage('invitation')
+  async handleInvitation(client: any, payload: any) {
+    let socket_id = payload[0].SOCKET;
+    if (payload[0].TYPE == "invitation") {
+      if (this.games.has(socket_id))
+        this.games.delete(socket_id);
+      this.games.set(socket_id, new Game(payload[0].USER, "white"));
+      this.games.get(socket_id).gameState = GameState.WAITINGFOR;
+      this.games.get(socket_id).waitedPseudo = payload[0].USER;
+    }
+    else if (payload[0].TYPE == "declined") {
+      this.games.get(socket_id).gameState = GameState.DECLINED;
+    }
+    if (payload[0].TYPE == "accepted") {
+      if (this.games.has(socket_id))
+        this.games.delete(socket_id);
+      this.games.set(socket_id, new Game(payload[0].USER, "white"));
+      this.games.get(socket_id).gameState = GameState.SEARCHINGFOR;
+    }
+  }
+
+  @SubscribeMessage('spectate')
+  async handleSpec(client: any, payload: any) {
+    let verif = await this.prisma.ongoingGame.findFirst({
+      where: {
+        OR: [
+          {JOUEUR_1_PSEUDO : payload[0].USER},
+          {JOUEUR_2_PSEUDO : payload[0].USER},
+        ]
+      }
+    });
+    if (Object.keys(verif).length == 0) {
+      client.emit('spectate', {
+        ISGAME: false,
+      });
+    }
+    else {
+      client.emit('spectate', {
+        ISGAME: true,
+      });
+      let tmp = 0;
+      let socket_id = payload[0].SOCKET;
+      for (let [key, value] of this.games) {
+        if (value.playerOneName == payload[0].USER || value.playerTwoName == payload[0].USER) {
+          this.games.set(socket_id, value);
+          break ;
+        }
+      }
+    }
+  }
+  
 
   @SubscribeMessage('update')
   async handleAction(client: any, payload: any) {
@@ -30,6 +82,7 @@ export class PongGateway {
       else if (this.games.get(socket_id).gameState == GameState.OPTION) {
         this.games.get(socket_id).changeOptionMenu(payload[0]);
         client.emit('update', this.games.get(socket_id).returnData());
+        console.log(this.games.get(socket_id).returnData());
       }
       else if (this.games.get(socket_id).gameState == GameState.WAITING) {
         if (payload[0].ACTION == "QUIT") {
@@ -42,13 +95,41 @@ export class PongGateway {
           this.games.get(socket_id).gameState = GameState.MENU;
         }
       }
+      else if (this.games.get(socket_id).gameState == GameState.SEARCHINGFOR) {
+        for (let [key, value] of this.games) {
+          if (value.data_multi == this.games.data_multi) {
+            this.games.get(key).addPlayer(payload[0], user_id);
+            this.games.delete(socket_id);
+            this.games.set(socket_id, this.games.get(key));
+            this.games.get(key).gameState = GameState.MULTI;
+            await this.prisma.ongoingGame.create({
+              data: {
+                JOUEUR_1_SOCKET : key,
+                JOUEUR_1_PSEUDO : this.games.get(key).playerOneName,
+                JOUEUR_2_SOCKET : socket_id,
+                JOUEUR_2_PSEUDO : user_id,
+              }
+            });
+          }
+        }
+        this.games.get(socket_id).update(payload[0]);
+        client.emit('update', this.games.get(socket_id).returnData());
+      }
       else if (this.games.get(socket_id).gameState == GameState.SEARCHING) {
         for (let [key, value] of this.games) {
           if (key != socket_id && value.gameState == GameState.WAITING) {
             this.games.get(key).addPlayer(payload[0], user_id);
-            this.games.delete(socket_id, Game);
+            this.games.delete(socket_id);
             this.games.set(socket_id, this.games.get(key));
             this.games.get(key).gameState = GameState.MULTI;
+            await this.prisma.ongoingGame.create({
+              data: {
+                JOUEUR_1_SOCKET : key,
+                JOUEUR_1_PSEUDO : this.games.get(key).playerOneName,
+                JOUEUR_2_SOCKET : socket_id,
+                JOUEUR_2_PSEUDO : user_id,
+              }
+            });
           }
           if (this.games.get(socket_id).gameState == GameState.SEARCHING) {
             this.games.get(socket_id).gameState = GameState.WAITING;
@@ -94,7 +175,6 @@ export class PongGateway {
         if (payload[0].ACTION == "GO") {
           tmp = 0;
           for (let [key, value] of this.games) {
-            console.log(value.galeState);
             tmp++;
             if (this.games.get(socket_id).specMenuState != 0) {
               this.games.get(socket_id).gameState == GameState.SPECTATING;
@@ -115,6 +195,17 @@ export class PongGateway {
         }
         client.emit('update', this.games.get(socket_id).returnData());
       }
+      else if (this.games.get(socket_id).gameState == GameState.DECLINED) {
+        if (payload[0].ACTION == "QUIT") {
+          let color = "white";
+          if (this.games.has(socket_id)) {
+            color = this.games.get(socket_id).color;
+            this.games.delete(socket_id);
+          }
+          this.games.set(socket_id, new Game(socket_id, color));
+          this.games.get(socket_id).gameState = GameState.MENU;
+        }
+      }
       else if (this.games.get(socket_id).gameState == GameState.OVER) {
         if (payload[0].ACTION == "QUIT") {
           let color = "white";
@@ -126,6 +217,27 @@ export class PongGateway {
           this.games.get(socket_id).gameState = GameState.MENU;
         }
         this.games.get(socket_id).update(payload[0]);
+        client.emit('update', this.games.get(socket_id).returnData());
+      }
+      else if (this.games.get(socket_id).gameState == GameState.DESTROYINGGAME) {
+        this.games.get(socket_id).gameState = GameState.OVER;
+        await this.prisma.ongoingGame.deleteMany({
+          where: {
+            JOUEUR_1_SOCKET: socket_id,
+          }
+        })
+        await this.prisma.ongoingGame.deleteMany({
+          where: {
+            JOUEUR_2_SOCKET: socket_id,
+          }
+        })
+        await this.prisma.gameHistory.create({
+          data: {
+            JOUEUR_1: this.games.get(socket_id).playerOneName,
+            JOUEUR_2: this.games.get(socket_id).playerTswoName,
+            VAINQUEUR: this.games.get(socket_id).winner,
+          }
+        })
         client.emit('update', this.games.get(socket_id).returnData());
       }
       else {
