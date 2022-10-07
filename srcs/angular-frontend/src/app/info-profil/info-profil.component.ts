3544beed3a14700483a72108c57a070d5f9a8536
arapaill <arapaill@student.s19.be>
2022-10-07 05:37:24 +0200

    friends update

--- srcs/angular-frontend/src/app/info-profil/info-profil.component.ts
+++ srcs/angular-frontend/src/app/info-profil/info-profil.component.ts
@@ -14,34 +14,31 @@ import { myUser, User} from '../models/user.model';
 })
 export class InfoProfilComponent implements OnInit {
   Personne!: ProfileModel;
-  percentDone!: number;
-  uploadSuccess!: boolean;
   matchs !: string[];
   nameProfil !: string;
-  userID : number = 0;
-  constructor(private webSocketService: WebSocketService, private  dialogRef : MatDialog, @Inject(MAT_DIALOG_DATA) public data : any, public myUser : myUser) {
+  constructor(private webSocketService: WebSocketService, private  dialogRef : MatDialog, @Inject(MAT_DIALOG_DATA) public data : any, public myUser : myUser, public dude : myUser) {
     this.nameProfil = data.name;
 
   }
 
   ngOnInit(): void {
+    
     this.webSocketService.emit("requestUserInfos", this.nameProfil);
     this.webSocketService.listen("getUserInfos").subscribe((data: any) => {
-      this.myUser.avatar = data.avatar;
-      this.myUser.pseudo = data.name;
-      this.myUser.description = data.Description;
-      this.myUser.blacklist = data.blacklist;
-      this.myUser.id = data.id;
+      this.dude.avatar = data.avatar;
+      this.dude.pseudo = data.name;
+      this.dude.description = data.Description;
+      this.dude.id = data.id;
 
       this.Personne = 
       {
-        avatar: this.myUser.avatar,
-        name:  this.myUser.pseudo,
-        description: this.myUser.description,
+        avatar: this.dude.avatar,
+        name:  this.dude.pseudo,
+        description: this.dude.description,
         date: new Date(),
         victoires: 0,
         match: false,
-        id :this.myUser.id
+        id :this.dude.id
       }
     });
  
@@ -57,12 +54,28 @@ export class InfoProfilComponent implements OnInit {
   } //end of ngoninit
   
   addToFriends() {
-    if (!this.myUser.friends.has(this.Personne.name)) {
-      this.myUser.friends.set(this.Personne.name, this.userID);
-      this.myUser.friends = this.myUser.friends;
+    this.webSocketService.emit("requestUserInfosID", Number(localStorage.getItem('id')));
+    this.webSocketService.listen("getUserInfosID").subscribe((data: any) => {
+      this.myUser.avatar = data.avatar;
+      this.myUser.pseudo = data.name;
+      this.myUser.description = data.Description;
+      this.myUser.blacklist = data.blacklist;
+      this.myUser.id = data.id;
+      data.friends.forEach((key : any, value : any) : any =>{
+        this.myUser.friends.push(value, key);
+      });
+    
+    if (!this.myUser.friends.indexOf(this.Personne.id)) {
+      this.myUser.friends.push(this.Personne.id);
     }
     else
-      this.myUser.friends.delete(this.Personne.name);
-    this.webSocketService.emit("updateFriendlist", this.userID);
+      {
+        let ret = this.myUser.friends.indexOf(this.Personne.id)
+        this.myUser.friends.splice(ret, 1);
+      }
+    this.webSocketService.emit("updateUser", this.myUser);
+    console.log(this.myUser);
+  });
   }
+
 }
--- srcs/angular-frontend/src/app/popup-display-friends/popup-display-friends.component.ts
+++ srcs/angular-frontend/src/app/popup-display-friends/popup-display-friends.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { InfoProfilComponent } from '../info-profil/info-profil.component';
 import { myUser, User } from '../models/user.model';
+import { WebSocketService } from '../web-socket.service';
 
 
 @Component({
@@ -10,20 +11,28 @@ import { myUser, User } from '../models/user.model';
   styleUrls: ['./popup-display-friends.component.scss']
 })
 export class PopupDisplayFriendsComponent implements OnInit {
-  friends : string[] = [];
-  constructor(private  dialogRef : MatDialog, public myUser : myUser) { }
+  friends : number[] = [];
+  constructor(private  dialogRef : MatDialog, public myUser : myUser, private webSocketService: WebSocketService) { }
  
   ngOnInit(): void {
-    this.myUser.friends.set("Arapaill", 0);
-    this.myUser.friends.set("Cgoncalv", 2);
-    this.myUser.friends.forEach((key,value) : any =>{
-      this.friends.push(value);
+    this.webSocketService.emit("requestUserInfosID", Number(localStorage.getItem('id')));
+    this.webSocketService.listen("getUserInfosID").subscribe((data: any) => {
+      this.myUser.avatar = data.avatar;
+      this.myUser.pseudo = data.name;
+      this.myUser.description = data.Description;
+      this.myUser.blacklist = data.blacklist;
+      this.myUser.id = data.id;
+      data.friends.forEach((key : number, value : any) : any =>{
+        this.friends.push(key);
+      });
+      console.log(data.friends);
     });
+    
   }
-  openDialog(nom: string){
+  openDialog(num : number){
     this.dialogRef.open(InfoProfilComponent,{
       data : {
-        name : nom,
+        num : num,
       }
     });
   }
