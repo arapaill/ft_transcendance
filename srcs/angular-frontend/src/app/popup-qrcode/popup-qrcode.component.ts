d29640eb353e93ff9e4a68c0e7f6de7d1126b6ff
arapaill <arapaill@student.s19.be>
2022-10-09 15:49:09 +0200

    popup qr

---
+++ srcs/angular-frontend/src/app/popup-qrcode/popup-qrcode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-qrcode',
+  templateUrl: './popup-qrcode.component.html',
+  styleUrls: ['./popup-qrcode.component.scss']
+})
+export class PopupQrcodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
